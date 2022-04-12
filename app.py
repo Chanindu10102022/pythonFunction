49df894c7f14b2ed2bfac53b5936f269892de3e4
chanindu2022Python <103425107+Chanindu10102022@users.noreply.github.com>
2022-04-12 09:52:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# for look my features visit now >> chanindu10102022 https://github.com/Chanindu10102022/
+
+# first you need to type def in you're vs code
+# the hit tab
+# use you'r favorite name for this funtion I am use function1
+# now run
+# you can see there hava nothing
+# let define the function to defing the funtion you need to call the functon
+# if it we save you can see the "function1()" gose to 2 lines or 3 lines
+# let save
+# I think you saw that
+
+# lets coustemise the function
+# thats ect ...... lets run the funtion
+# super cool plz share my github link to you're friend and lovers
+# thank you !!!
+
+
+def function1(name):
+    print(f"my name is {name}")
+
+
+function1("chanindu")
